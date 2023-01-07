aabd5210a84768757caa03a9c5175328ebfde47e
Arttu Tamminen <arttu.tamminen@kapsi.fi>
2023-01-07 18:33:22 +0200

    1498 bytes milestone, had to reduce graphics

--- sourceDev.js
+++ sourceDev.js
@@ -2,31 +2,31 @@ c = document.body.appendChild(document.createElement("canvas"));
 c.width = 640;
 c.height = 360;
 
-gl = c.getContext("webgl");
+g = c.getContext("webgl");
 var vertexShader = `attribute vec3 position;void main(){gl_Position = vec4(position, 1.0);}`;
 //Remember to change time to be uniform float v and resolution to 640x360
 //First two lines should be like precision highp float;uniform float v;
-var fragmentShader = `precision highp float;uniform float v;struct v3{vec3 fr;vec3 sd;};struct en{float d;vec3 po;vec3 n;vec4 m1;vec4 m2;};struct ht{float d;en en;};vec2 n;en s(en n,en d){if(n.d<d.d)return n;return d;}vec3 e(vec3 v,vec3 n){float s=sin(n.z),e=cos(n.z);vec3 d=vec3(e*v.x-s*v.y,s*v.x+e*v.y,v.z);float m=sin(n.y),y=cos(n.y);vec3 f=vec3(y*d.x+m*d.z,d.y,-m*d.x+y*d.z);float x=sin(n.x),l=cos(n.x);return vec3(f.x,l*f.y-x*f.z,x*f.y+l*f.z);}v3 f(vec3 v,vec3 m){return v3(mod(v+m*.5,m)-m*.5,floor((v+m*.5)/m));}en e(vec3 v,vec3 d,vec4 n,vec4 m){en f;vec3 e=abs(v)-d;f.d=min(max(e.x,max(e.y,e.z)),0.)+length(max(e,0.))-.3;f.m1=n;f.m2=m;return f;}en e(vec3 m){float d=v/2.,y=v*10.;v3 r=f(e(m,vec3(0.,d,3.2)),vec3(3.5,0.,3.5));float x=sin((r.sd.x+y)/10.)*sin((r.sd.z+y)/5.);en a=e(e(r.fr-vec3(0.,sin(v)*10.+x*10.+25.,0.),vec3(sin(v)*4.)*x),vec3(1.),vec4(.01,0.,.17,.25),vec4(.95,.73,.77,.05));r=f(e(m,vec3(d,v,-v)),vec3(75.));en z=e(e(r.fr,vec3(r.sd)+v)+length(sin((n.xy+v)*13.)),vec3(10.),vec4(abs(r.sd.xyz),.05),vec4(vec3(1.),.1));return s(a,z);}ht f(vec3 m){ht v;for(int f=0;f<=64;f++){vec3 d=vec3(0.,30.,50.)+m*v.d;v.en=e(d);v.d+=v.en.d;if(abs(v.en.d)<.001){vec2 n=vec2(1e-4,0);v.en.n=normalize(vec3(e(d+n.xyy).d-e(d-n.xyy).d,e(d+n.yxy).d-e(d-n.yxy).d,e(d+n.yyx).d-e(d-n.yyx).d));break;}}return v;}void main(){vec2 v=vec2(640,360);n=gl_FragCoord.xy/v.xy*2.-1.;n.x*=v.x/v.y;ht d=f(normalize(vec3(0.,-.5145,-.8575)+n.x*vec3(-.75,0.,0.)+.75*n.y*vec3(0.,.8575,-.5145)));gl_FragColor=vec4(vec3(1.-smoothstep(0.,300.,d.d))*(d.en.m1.xyz*d.en.m1.w*10.+max(dot(d.en.n,normalize(vec3(0.,100.,10.)-d.en.po)),0.)*d.en.m2.xyz*d.en.m2.w*10.),1.);}`;
+var fragmentShader = `precision highp float;uniform float v;struct v3{vec3 fr;vec3 sd;};struct en{float d;vec3 po;vec3 n;vec4 m1;vec4 m2;};struct ht{float d;en en;};vec2 n;vec3 s(vec3 n,vec3 v){float m=sin(v.z),s=cos(v.z);vec3 d=vec3(s*n.x-m*n.y,m*n.x+s*n.y,n.z);float e=sin(v.y),y=cos(v.y);vec3 a=vec3(y*d.x+e*d.z,d.y,-e*d.x+y*d.z);float f=sin(v.x),x=cos(v.x);return vec3(a.x,x*a.y-f*a.z,f*a.y+x*a.z);}v3 e(vec3 v,vec3 m){return v3(mod(v+m*.5,m)-m*.5,floor((v+m*.5)/m));}en e(vec3 m){v3 d=e(s(m,vec3(v/2.,v,-v)),vec3(75.));en f;vec3 a=abs(s(d.fr,vec3(d.sd)+v*2.)+length(sin((n.xy+v)*13.)))-vec3(10.);f.d=min(max(a.x,max(a.y,a.z)),0.)+length(max(a,0.))-.3;f.m1=vec4(abs(d.sd.xyz),.05);f.m2=vec4(vec3(1.),.1);return f;}ht s(vec3 m){ht v;for(int f=0;f<=64;f++){vec3 d=vec3(0.,30.,50.)+m*v.d;v.en=e(d);v.d+=v.en.d;if(abs(v.en.d)<.001){vec2 n=vec2(1e-4,0);v.en.n=normalize(vec3(e(d+n.xyy).d-e(d-n.xyy).d,e(d+n.yxy).d-e(d-n.yxy).d,e(d+n.yyx).d-e(d-n.yyx).d));break;}}return v;}void main(){vec2 v=vec2(640,360);n=gl_FragCoord.xy/v.xy*2.-1.;n.x*=v.x/v.y;ht d=s(normalize(vec3(0.,-.5145,-.8575)+n.x*vec3(-.75,0.,0.)+.75*n.y*vec3(0.,.8575,-.5145)));gl_FragColor=vec4(vec3(1.-smoothstep(0.,300.,d.d))*(d.en.m1.xyz*d.en.m1.w*10.+max(dot(d.en.n,normalize(vec3(0.,100.,10.)-d.en.po)),0.)*d.en.m2.xyz*d.en.m2.w*10.),1.);}`;
 
 function createShader(src, type) {
-    var shader = gl.createShader(type);
-    gl.shaderSource(shader, src);
-    gl.compileShader(shader);
+    var shader = g.createShader(type);
+    g.shaderSource(shader, src);
+    g.compileShader(shader);
     return shader;
 }
 
 function draw(time) {  
-    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(0);
+    g.vertexAttribPointer(0, 2, g.FLOAT, false, 0, 0);
+    g.enableVertexAttribArray(0);
   
-    gl.uniform1f(gl.getUniformLocation(program, "v"), time / 1000);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-    window.requestAnimationFrame(draw);
+    g.uniform1f(g.getUniformLocation(program, "v"), time / 1000);
+    g.drawArrays(g.TRIANGLE_STRIP, 0, 4);
+    requestAnimationFrame(draw);
 };
 
 V.onclick = () => {
-    const audioContext = new window.AudioContext();
-    const soundBuffer = audioContext.createBuffer(1, 22050 * 60, 44000);
+    var audioContext = new AudioContext();
+    var soundBuffer = audioContext.createBuffer(1, 22050 * 60, 44000);
 
     for (let t = 0; t < soundBuffer.length; t++) {
         //This tune is ripped from https://greggman.com/downloads/examples/html5bytebeat/html5bytebeat.htm
@@ -37,7 +37,7 @@ V.onclick = () => {
         & 255) / 127 - 1;
     }
 
-    const soundSource = audioContext.createBufferSource();
+    var soundSource = audioContext.createBufferSource();
     
     soundSource.buffer = soundBuffer;
     soundSource.connect(audioContext.destination);
@@ -47,16 +47,16 @@ V.onclick = () => {
     c.style.cursor = 'none';
     c.onclick = null;
 
-    var ab = gl.ARRAY_BUFFER;
-    gl.bindBuffer(ab, gl.createBuffer());
-    gl.bufferData(ab, new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]), gl.STATIC_DRAW);
+    var ab = g.ARRAY_BUFFER;
+    g.bindBuffer(ab, g.createBuffer());
+    g.bufferData(ab, new Float32Array([1, 1, -1, 1, 1, -1, -1, -1]), g.STATIC_DRAW);
 
-    program = gl.createProgram();
+    program = g.createProgram();
 
-    gl.attachShader(program, createShader(vertexShader, gl.VERTEX_SHADER));
-    gl.attachShader(program, createShader(fragmentShader, gl.FRAGMENT_SHADER));
+    g.attachShader(program, createShader(vertexShader, g.VERTEX_SHADER));
+    g.attachShader(program, createShader(fragmentShader, g.FRAGMENT_SHADER));
 
-    gl.linkProgram(program);
-    gl.useProgram(program);
-    window.requestAnimationFrame(draw);
+    g.linkProgram(program);
+    g.useProgram(program);
+    requestAnimationFrame(draw);
 };
--- tools/src/shaders/fragment.frag
+++ tools/src/shaders/fragment.frag
@@ -22,12 +22,6 @@ struct ht {
 };
 vec2 uv;
 
-en opUnion(en m1, en m2) {
-    if (m1.d < m2.d)
-        return m1;
-    return m2;
-}
-
 vec3 rot(vec3 zp, vec3 a) {
     float zs = sin(a.z);
     float zc = cos(a.z);
@@ -55,36 +49,14 @@ v3 repeat(vec3 p, vec3 size) {
     return v3(mod(p + size * 0.5, size) - size * 0.5, floor((p + size * 0.5) / size));
 }
 
-en mBox(vec3 path, vec3 size, vec4 m1, vec4 m2) {
-    en m;
-    vec3 d = abs(path) - size;
-    m.d = (min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0)) - 0.3);
-    m.m1 = m1;
-    m.m2 = m2;
-    return m;
-}
-
 en scene(vec3 path) {    
-    float time3 = time / 2.0;
-    float time2 = time * 10.0;
-    v3 boxR = repeat(rot(path, vec3(0.0, time3, 3.2)), vec3(3.5, 0.0, 3.5));
-    
-    float d = sin((boxR.sd.x + time2) / 10.0) * sin((boxR.sd.z + time2) / 5.0);
-    en waves = mBox(
-        rot(boxR.fr - vec3(0.0, (sin(time) * 10.0) + d * 10.0 + 25.0, 0.0), vec3(sin(time) * 4.0) * d),
-        vec3(1.0),
-        vec4(0.01, 0.0, 0.17, 0.25),
-        vec4(0.95, 0.73, 0.77, 0.05)
-    );
-
-    boxR = repeat(rot(path, vec3(time3, time, -time)), vec3(75.0));
-    en cubes = mBox(
-        rot(boxR.fr, vec3(boxR.sd) + time) + length(sin((uv.xy + time) * 13.0)),
-        vec3(10.0),
-        vec4(abs(boxR.sd.xyz), 0.05),
-        vec4(vec3(1.0), 0.1)
-    );
-    return opUnion(waves, cubes);
+    v3 boxR = repeat(rot(path, vec3(time / 2.0, time, -time)), vec3(75.0));
+    en cubes;
+    vec3 d = abs(rot(boxR.fr, vec3(boxR.sd) + time * 2.0) + length(sin((uv.xy + time) * 13.0))) - vec3(10.0);
+    cubes.d = (min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0)) - 0.3);
+    cubes.m1 = vec4(abs(boxR.sd.xyz), 0.05);
+    cubes.m2 = vec4(vec3(1.0), 0.1);
+    return cubes;
 } 
 
 ht raymarch(vec3 rd) {
